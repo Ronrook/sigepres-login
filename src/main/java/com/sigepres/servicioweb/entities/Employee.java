47649b587e4065b2bb69b2d76feb67ab68ad23c8
Ronald Rosero <ronaldes93@gmail.com>
2023-10-26 14:47:30 -0500

    Class Employee implements UserDetails and its methods

--- src/main/java/com/sigepres/servicioweb/entities/Employee.java
+++ src/main/java/com/sigepres/servicioweb/entities/Employee.java
@@ -1,16 +1,25 @@
 package com.sigepres.servicioweb.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "employees")
-public class Employee implements Serializable {
+public class Employee implements Serializable, UserDetails {
     @Id
     @Column(name = "user_id")
     private Integer employeeId;
@@ -26,7 +35,32 @@ public class Employee implements Serializable {
     @JoinColumn(name = "user_id", referencedColumnName = "user_id", insertable = false, updatable = false)
     private User user;
 
-    public Employee() {
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority((role.getRoleName())));
     }
 
+    @Override
+    public String getUsername() {
+        return this.user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
