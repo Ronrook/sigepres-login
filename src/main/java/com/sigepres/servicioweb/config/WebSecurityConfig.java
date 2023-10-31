fa9f794bfb4869527b72426470b00b658b6ea527
Ronald Rosero <ronaldes93@gmail.com>
2023-10-31 17:53:50 -0500

    add WebSecurityConfig and ApplicationConfig

--- src/main/java/com/sigepres/servicioweb/config/WebSecurityConfig.java
+++ src/main/java/com/sigepres/servicioweb/config/WebSecurityConfig.java
@@ -1,14 +1,18 @@
 package com.sigepres.servicioweb.config;
 
 
+import com.sigepres.servicioweb.jwt.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
 /**
@@ -19,23 +23,31 @@ import org.springframework.security.web.SecurityFilterChain;
 @RequiredArgsConstructor // Anotación para crear el contructor con todos los parametros
 public class WebSecurityConfig {
 
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final AuthenticationProvider authProvider;
+
+
     /**
      * Configura la seguridad de las peticiones HTTP
      * @param http Peticion a configurar
-     * @return
+     * @return SecurityFilterChain
      * @throws Exception
      */
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         return http
             .csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(authRequest ->
                 authRequest
-                    .requestMatchers("/auth/**").permitAll() // permite el acceso a cualquier ruta que comience con este String
+                    .requestMatchers("/auth/login").permitAll() // permite el acceso a cualquier ruta que comience con este String
                     .anyRequest().authenticated() // Otras rutas se deben autenticar
             )
-            .formLogin(Customizer.withDefaults())
-            .build();
+                .sessionManagement(sessionManager->
+                    sessionManager
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authProvider)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
     }
 }
