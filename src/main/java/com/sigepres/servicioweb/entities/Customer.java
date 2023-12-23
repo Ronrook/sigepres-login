ee7de9dc2e3ffe3f8443ef6652a4c99e1f70380e
Ronald Rosero <ronaldes93@gmail.com>
2023-12-23 18:57:41 -0500

    @NoArgsConstructor annotation removed

--- src/main/java/com/sigepres/servicioweb/entities/Customer.java
+++ src/main/java/com/sigepres/servicioweb/entities/Customer.java
@@ -3,20 +3,16 @@ package com.sigepres.servicioweb.entities;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.io.Serializable;
 
 @Getter
 @Setter
-@NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @PrimaryKeyJoinColumn(name = "customer_id")
 @Table(name = "customers")
 public class Customer extends User implements Serializable {
-    // Atributos propios de la clase
-
 
 }
