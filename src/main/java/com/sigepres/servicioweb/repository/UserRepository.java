1d6bba4b5fe7a6e4788589eb9da9f430df11ff54
Ronald Rosero <ronaldes93@gmail.com>
2023-12-23 18:55:25 -0500

    UserRepository is added

--- src/main/java/com/sigepres/servicioweb/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sigepres.servicioweb.repository;
-
-import com.sigepres.servicioweb.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
