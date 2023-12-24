b96cd5866f0486abc632d0f0972f885a63c7f61b
Ronald Rosero <ronaldes93@gmail.com>
2023-12-23 19:09:36 -0500

    mapping path is changed in AuthController

--- src/main/java/com/sigepres/servicioweb/controller/AuthController.java
+++ src/main/java/com/sigepres/servicioweb/controller/AuthController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthController {
 
