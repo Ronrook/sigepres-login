e139a427025320fbeb907cefab2efae038d3fed7
Ronald Rosero <ronaldes93@gmail.com>
2023-12-23 19:21:08 -0500

    AppointmentRequestDTO is added

---
+++ src/main/java/com/sigepres/servicioweb/dto/AppointmentRequestDTO.java
@@ -0,0 +1,40 @@
+package com.sigepres.servicioweb.dto;
+
+
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+
+import java.sql.Time;
+import java.time.LocalDate;
+
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class AppointmentRequestDTO {
+
+    private Integer appointmentId;
+
+    @NotNull(message = "La fecha de la cita no puede ser nula.")
+    @Future(message = "La fecha de la cita debe ser en el futuro.")
+    private LocalDate appointmentDate;
+
+    @NotNull(message = "La de inicio de la cita no puede ser nula.")
+    private Time appointmentStartTime;
+
+    @NotNull(message = "La de final de la cita no puede ser nula.")
+    private Time appointmentEndTime;
+
+    @NotNull(message = "El ID del cliente no puede ser nulo.")
+    private Integer customerId;
+
+    @NotNull(message = "El ID del servicio no puede ser nulo.")
+    private Integer serviceId;
+
+    @NotNull(message = "El ID del empleado no puede ser nulo.")
+    private Integer employeeId;
+}
