8e41d382fe5721649b623d14518cbb5dc3eef5ee
Ronald Rosero <ronaldes93@gmail.com>
2023-12-23 19:18:12 -0500

    IApppointmentService is added

---
+++ src/main/java/com/sigepres/servicioweb/service/IAppointmentService.java
@@ -0,0 +1,34 @@
+package com.sigepres.servicioweb.service;
+
+import com.sigepres.servicioweb.dto.AppointmentRequestDTO;
+import com.sigepres.servicioweb.dto.AppointmentResponseDTO;
+import com.sigepres.servicioweb.entities.Appointment;
+
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface IAppointmentService {
+
+    AppointmentResponseDTO createAppointment(AppointmentRequestDTO appointmentRequestDTO);
+
+    AppointmentResponseDTO getAppointmentById(Integer appointmentId);
+
+    List<AppointmentResponseDTO> getAppointmentsByEmployeeId(Integer employeeId);
+
+    List<AppointmentResponseDTO> getAppointmentsByCustomerId(Integer customerId);
+
+    List<AppointmentResponseDTO> getAppointmentsBetweenDates(LocalDate startDate, LocalDate endDate);
+
+    List<AppointmentResponseDTO> getAppointmentsByCustomerAndDates(Integer customerId, LocalDate startDate, LocalDate endDate);
+
+    List<AppointmentResponseDTO> getAppointmentsByEmployeeAndDates(Integer employeeId, LocalDate startDate, LocalDate endDate);
+
+    List<AppointmentResponseDTO> getAppointmentsByCustomer(Integer customerId);
+
+    AppointmentResponseDTO updateAppointment(int appointmentId, Appointment updatedAppointment);
+
+    void deleteAppointment(int appointmentId);
+
+
+}
