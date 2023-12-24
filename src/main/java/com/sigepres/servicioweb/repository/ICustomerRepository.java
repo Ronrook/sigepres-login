abd502b99578238cffca27ac6cda4eb11061c746
Ronald Rosero <ronaldes93@gmail.com>
2023-12-23 19:08:23 -0500

    CustomerRepository is renamed to ICustomerRepository

--- src/main/java/com/sigepres/servicioweb/repository/ICustomerRepository.java
+++ src/main/java/com/sigepres/servicioweb/repository/ICustomerRepository.java
@@ -3,5 +3,5 @@ package com.sigepres.servicioweb.repository;
 import com.sigepres.servicioweb.entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CustomerRepository extends JpaRepository <Customer, Integer> {
+public interface ICustomerRepository extends JpaRepository <Customer, Integer> {
 }
