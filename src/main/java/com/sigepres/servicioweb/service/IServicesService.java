edd96f9ea03d2631aea875b6332c6d41114f3b6b
Ronald Rosero <ronaldes93@gmail.com>
2023-12-23 20:53:40 -0500

    add IServicesService

---
+++ src/main/java/com/sigepres/servicioweb/service/IServicesService.java
@@ -0,0 +1,8 @@
+package com.sigepres.servicioweb.service;
+
+import com.sigepres.servicioweb.entities.Services;
+
+public interface IServicesService {
+
+    Services getServicesById(Integer serviceId);
+}
