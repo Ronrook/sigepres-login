ab0a3b43ad418b3d1b2c99778ceed662751ce8f7
Ronald Rosero <ronaldes93@gmail.com>
2023-12-23 20:54:18 -0500

    ServicesServiceImpl implements IServicesService

---
+++ src/main/java/com/sigepres/servicioweb/service/ServicesServiceImpl.java
@@ -0,0 +1,24 @@
+package com.sigepres.servicioweb.service;
+
+import com.sigepres.servicioweb.entities.Services;
+import com.sigepres.servicioweb.exceptions.ResourceNotFoundException;
+import com.sigepres.servicioweb.repository.IServicesRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ServicesServiceImpl implements IServicesService{
+
+    private final IServicesRepository serviceRepository;
+
+    public ServicesServiceImpl(IServicesRepository serviceRepository) {
+        this.serviceRepository = serviceRepository;
+    }
+
+    @Override
+    public Services getServicesById(Integer serviceId) {
+        return serviceRepository
+                .findById(serviceId)
+                .orElseThrow(
+                        () -> new ResourceNotFoundException("El servicio ingresado no existe."));
+    }
+}
