0e5a4e0fd54ad07378e210734be6b47267d59010
Ronald Rosero <ronaldes93@gmail.com>
2024-01-08 20:13:22 -0500

    routes are created for the AppointmentController methods

--- src/main/java/com/sigepres/servicioweb/controller/AppointmentController.java
+++ src/main/java/com/sigepres/servicioweb/controller/AppointmentController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -29,8 +30,50 @@ public class AppointmentController {
     public ResponseEntity<AppointmentResponseDTO> getAppointment(@PathVariable("id") Integer appointmentId){
         return new ResponseEntity<>(appointmentService.getAppointmentById(appointmentId), HttpStatus.OK);
     }
-    @GetMapping("/appointments/{id}")
-    public  ResponseEntity<List<AppointmentResponseDTO>> getAppointmentsByEmployeeId(@PathVariable("id") Integer employeeId) {
-        return new ResponseEntity<>(appointmentService.getAppointmentsByEmployeeId(employeeId), HttpStatus.OK);
+
+    @GetMapping("/appointments/employee/{id}")
+    public  ResponseEntity<List<AppointmentResponseDTO>> getAppointmentsByEmployeeId(
+            @PathVariable("id") Integer employeeId,
+            @RequestParam(required = false) LocalDate startDate,
+            @RequestParam(required = false) LocalDate endDate) {
+
+        if (startDate == null || endDate == null){
+            return new ResponseEntity<>(appointmentService.getAppointmentsByEmployeeId(employeeId), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(appointmentService.getAppointmentsByEmployeeAndDates(employeeId, startDate, endDate), HttpStatus.OK);
+    }
+
+    @GetMapping("/appointments/customer/{id}")
+    public  ResponseEntity<List<AppointmentResponseDTO>> getAppointmentsByCustomerId(
+            @PathVariable("id") Integer customerId,
+            @RequestParam(required = false) LocalDate startDate,
+            @RequestParam(required = false) LocalDate endDate) {
+
+        if (startDate == null || endDate == null){
+            return new ResponseEntity<>(appointmentService.getAppointmentsByCustomerId(customerId), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(appointmentService.getAppointmentsByCustomerAndDates(customerId, startDate, endDate), HttpStatus.OK);
+
+    }
+
+    @GetMapping("/appointments")
+    public  ResponseEntity<List<AppointmentResponseDTO>> getAppointmentsBetweenDates(
+            @RequestParam(required = false) LocalDate startDate,
+            @RequestParam(required = false) LocalDate endDate) {
+
+        System.out.println("startDate = " + startDate);
+        System.out.println("endDate = " + endDate);
+
+        if (startDate == null || endDate == null){
+            // Lógica cuando no se proporcionan fechas, devuelve todas las citas.
+            return new ResponseEntity<>(appointmentService.getAllAppointments(), HttpStatus.OK);
+        }
+        // // Manejar el caso cuando se proporcionan ambas fechas.
+        return new ResponseEntity<>(appointmentService.getAppointmentsBetweenDates(startDate, endDate), HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<AppointmentResponseDTO> updateAppointment(@PathVariable("id") Integer appointmentId,                                            @Valid @RequestBody AppointmentRequestDTO appointmentRequestDTO){
+        return new ResponseEntity<>(appointmentService.updateAppointment(appointmentId, appointmentRequestDTO), HttpStatus.OK);
     }
 }
