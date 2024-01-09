8a2a42054dda04dd476a0ede9623f77c7f58c1d5
Ronald Rosero <ronaldes93@gmail.com>
2024-01-08 20:21:07 -0500

    INoteService and NoteServiceImpl are created

---
+++ src/main/java/com/sigepres/servicioweb/service/INoteService.java
@@ -0,0 +1,7 @@
+package com.sigepres.servicioweb.service;
+
+import com.sigepres.servicioweb.entities.Note;
+
+public interface INoteService {
+
+}
---
+++ src/main/java/com/sigepres/servicioweb/service/NoteServiceImpl.java
@@ -0,0 +1,4 @@
+package com.sigepres.servicioweb.service;
+
+public class NoteServiceImpl implements INoteService{
+}
