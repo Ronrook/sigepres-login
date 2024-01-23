ff38c5cc03eafb57a2b4a9293d0bf677980ca2a1
Ronald Rosero <ronaldes93@gmail.com>
2024-01-23 10:47:57 -0500

    Methods are created to validate user existence by email and dniNumber

--- src/main/java/com/sigepres/servicioweb/repository/IUserRepository.java
+++ src/main/java/com/sigepres/servicioweb/repository/IUserRepository.java
@@ -3,13 +3,10 @@ package com.sigepres.servicioweb.repository;
 import com.sigepres.servicioweb.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 
 public interface IUserRepository extends JpaRepository<User, Integer> {
 
-    Optional<User> findByEmail(String email);
-    Optional<User> findByDniNumber(String dniNumber);
-
-    //Optional<User> findByUsername(String username);
+    boolean existsByEmail(String email);
+    boolean existsByDniNumber(String dniNumber);
 }
 
