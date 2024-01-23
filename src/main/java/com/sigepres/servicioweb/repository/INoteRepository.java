972b2625471eb2007656a1b8ff96f967b64ca3b3
Ronald Rosero <ronaldes93@gmail.com>
2024-01-23 12:17:10 -0500

    INoteRepository is added with search method

--- src/main/java/com/sigepres/servicioweb/repository/INoteRepository.java
+++ src/main/java/com/sigepres/servicioweb/repository/INoteRepository.java
@@ -1,7 +1,12 @@
 package com.sigepres.servicioweb.repository;
 
+
 import com.sigepres.servicioweb.entities.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface INoteRepository extends JpaRepository<Note, Integer> {
+
+    List<Note> findByMedicalRecordId(Integer medicalHistoryId);
 }
